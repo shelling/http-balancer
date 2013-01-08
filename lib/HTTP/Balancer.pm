6c897482fc62addcbe20d4089c264e9277f3f2fb
shelling <navyblueshellingford@gmail.com>
2013-01-08 17:22:55 +0800

    initial commit

---
+++ lib/HTTP/Balancer.pm
@@ -0,0 +1,32 @@
+package HTTP::Balancer;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+HTTP::Balancer -
+
+=head1 SYNOPSIS
+
+  use HTTP::Balancer;
+
+=head1 DESCRIPTION
+
+HTTP::Balancer is
+
+=head1 AUTHOR
+
+shelling E<lt>navyblueshellingford@gmail.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
