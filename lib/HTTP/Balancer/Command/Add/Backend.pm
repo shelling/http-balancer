0351b9f3aed5eb2af80129157979327112f3c9ba
shelling <navyblueshellingford@gmail.com>
2013-01-14 17:02:01 +0800

    backend should belong to one host

--- lib/HTTP/Balancer/Command/Add/Backend.pm
+++ lib/HTTP/Balancer/Command/Add/Backend.pm
@@ -3,10 +3,22 @@ use Modern::Perl;
 use Moose;
 with qw(HTTP::Balancer::Role::Command);
 
+has to => (
+    is       => "rw",
+    isa      => "Str",
+    required => 1,
+);
+
 sub run {
     my ($self, ) = @_;
+
+    my $host = $self->model("Host")->find(name => $self->to);
+
     $self->model("Backend")
-         ->new(address => $self->extra_argv->[2])
+         ->new(
+             address => $self->extra_argv->[2],
+             host_id => $host->id,
+         )
          ->save;
 }
 
