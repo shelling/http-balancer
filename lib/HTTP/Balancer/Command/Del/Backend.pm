0c5bf19cc41e014e6a04102360e53bcf80c9499d
shelling <navyblueshellingford@gmail.com>
2013-02-18 17:04:16 +0800

    Add::Backend and Del::Backend use name to search instance

--- lib/HTTP/Balancer/Command/Del/Backend.pm
+++ lib/HTTP/Balancer/Command/Del/Backend.pm
@@ -3,9 +3,23 @@ use Modern::Perl;
 use Moose;
 with qw(HTTP::Balancer::Role::Command);
 
+has name => (
+    is      => "rw",
+    traits  => [ 'NoGetopt' ],
+    default => sub {
+        my $self = shift;
+        $self->extra_argv->[2] or do {
+            print $self->usage;
+            exit;
+        }
+    },
+);
+
 sub run {
     my ($self, ) = @_;
 
+    my $backend = $self->model("Backend")->find(name => $self->name);
+    $backend ? $backend->remove : say "not found";
 }
 
 
